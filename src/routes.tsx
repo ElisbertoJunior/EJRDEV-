76e9ee6fd97f10f3278962172c73afe3b1ffe564
Elisberto Junior <elisberto_jr@hotmail.com>
2023-06-22 09:17:08 -0300

    feat: add auto scroll

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,33 @@
-import { Routes as Router, Route } from 'react-router-dom'
+import { Routes as Router, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 import Login from './pages/Login'
 import Admin from './pages/Admin'
+import { useEffect } from 'react'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
 
 const Routes = () => {
   return (
-    <Router>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/admin" element={<Admin />} />
-    </Router>
+    <>
+      <ScrollToTop />
+      <Router>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+      </Router>
+    </>
   )
 }
 
