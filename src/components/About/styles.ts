7c1cfacab9a104c5343125231e26ba33813f7483
Elisberto Junior <elisberto_jr@hotmail.com>
2023-06-23 08:44:17 -0300

    teste

--- src/components/About/styles.ts
+++ src/components/About/styles.ts
@@ -13,12 +13,13 @@ export const AboutContent = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    p {
+
+    > p {
       width: 100%;
       text-align: center;
     }
 
-    ul {
+    > ul {
       display: flex;
       align-items: center;
       justify-content: center;
